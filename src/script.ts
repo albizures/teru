e6d2c776e408b06f4f7caeed31db4a3920a50243
Jose Albizures <albizures3601@gmail.com>
2020-05-02 23:20:45 +0300

    feat(cli): add verbose flag

--- src/script.ts
+++ src/script.ts
@@ -4,21 +4,34 @@ import meow from 'meow';
 import App from './index';
 import hostedGitInfo from 'hosted-git-info';
 
-const cli = meow(`
+const cli = meow(
+	`
 	Usage
-	  $ teru <name> <starter>
+		$ teru <name> <starter>
+		
+	Options
+		--verbose, -v
 
 	Examples
 	  $ teru my-app https://github.com/albizures/teru-node-starter
 	  $ teru my-app albizures/teru-node-starter
-`);
+`,
+	{
+		flags: {
+			verbose: {
+				type: 'boolean',
+				alias: 'v',
+			},
+		},
+	},
+);
 
 const run = () => {
 	const [name, starter] = cli.input;
 	const isValidStarter = Boolean(hostedGitInfo.fromUrl(starter));
 
 	if (name && starter && isValidStarter) {
-		render(React.createElement(App, { name, starter }));
+		render(React.createElement(App, { name, starter, ...cli.flags }));
 		return;
 	}
 
